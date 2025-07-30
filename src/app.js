d7426294890dfc038efe3b5ac375e0fe85607845
amanshu01 <aman2000.am@gmail.com>
2025-07-30 23:01:50 +0530

    correction

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://vite-frontend-ten.vercel.app"],
+    origin: ["http://localhost:5173","https://vite-frontend-ten.vercel.app",'https://www.estrellaco.com'],
   })
 );
 app.use( webhookRoutes);
